50f28f6ba79f4fbbebbe25888dd6c3a1a3d0c19f
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2022-11-09 14:58:10 +0500

    vs setup and Coal A2 done

--- MASM_1/COAL Assignment 2.asm
+++ MASM_1/COAL Assignment 2.asm
@@ -1,39 +1,48 @@
 include Irvine32.inc
 .data
-array1 SDWORD - 1, -2, 3, 9, -8, 8, 6, -5, -7
+array1 SDWORD -4, -3, -2, -1, 0, 1, 2, 3, 4
 array_Unsigned SDWORD SIZEOF array1 DUP(0)
+prompt1 BYTE "Org array values:",0
+prompt BYTE	"Unsigned values copied to Resultant array are:",0
 .code
 main proc
+mov edx, OFFSET prompt1
+call writestring
 mov ecx, lengthof array1
 mov esi, 0
 mov ebx, 0
-mov edx, 0
+mov edx, 0	;counter for unsigned values
 l1 :
+	mov al,9		;tab ascii
+	call writechar	;tab
+
 	mov eax, array1[esi]
-	or eax, 0	;to set/clear sign flag depanding on Value
-	JS skip
-		call writeInt	;block executed if sign flag set
-		call DumpRegs
+	call writeInt	
+	or eax, 0			;to set/clear sign flag depanding on Value
+	JS skip				;block executed if sign flag clear
 		mov array_Unsigned[ebx], eax	;copied to resultant array
 		add ebx, Type array1
 		inc edx						;count of unsigned values moved to resultant array
 	skip :
 	add esi, Type array1
 loop l1
-
 call crlf
 call crlf
-call crlf
-
 mov ecx, edx	;loop to be exexuted
 mov esi, 0
-l2 :
-mov eax, array_Unsigned[esi]
-call writeInt
-call DumpRegs
-; inc esi
-add esi, Type array1
 
+
+mov edx, OFFSET prompt
+call writeString
+mov al,9		;tab ascii
+call writechar	;tab
+mov esi, 0
+l2 :
+	mov eax, array_Unsigned[esi]
+	call writeInt
+	mov al,9		;tab ascii
+	call writechar	;tab
+	add esi, Type array1
 loop l2
 
 exit
--- MASM_1/Sample.asm
+++ MASM_1/Sample.asm
@@ -1,33 +1,25 @@
 ;TITLE Add and Subtract          (AddSub.asm)
-
 ; This program adds and subtracts 32-bit integers.
-
 INCLUDE Irvine32.inc
-
 .data
 i DWORD 0
 str1 BYTE "*",0
-
     num BYTE 126d
     num2 SBYTE -26d
     num3 WORD   692Ah
     num4 SWORD -32789
     num5 DWORD 12345678h
     num6 SDWORD -2147483648
-
 .code
 main PROC
-
 ;    mov al, num
 ;    call DumpRegs
-;
+
 ;    mov ah, num2
 ;    mov cx, num3
 ;    mov dx, num4
 ;    mov eax, num5
 ;    mov ebx, num6
-
-
 mov eax,i
 mov ecx, 5
 L1:
